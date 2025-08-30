770994a2cf92f8772daaaf1c735ced2fbec9e18b
ebirdeater <tawab.eashan.twb@gmail.com>
2025-08-30 22:53:48 +0600

    Fixes

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://glorious-elk-16.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
